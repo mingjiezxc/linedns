8c6937719ca13561fd13d6c2cead0f19d405d337
majorc <majorc@kali.mingjiezxc.work>
2021-08-31 01:51:53 -0400

    update

---
+++ go.mod
@@ -0,0 +1,8 @@
+module linedns
+
+go 1.16
+
+require (
+	github.com/gin-gonic/gin v1.7.4
+	github.com/miekg/dns v1.1.43 // indirect
+)
---
+++ main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"context"
+	"log"
+	"strconv"
+	"time"
+
+	"github.com/coreos/etcd/clientv3"
+	"github.com/gin-gonic/gin"
+	"github.com/miekg/dns"
+)
+
+var (
+	cli       *clientv3.Client
+	agentName = "test1"
+	zoneName  = "gz"
+	lineName  = "xx"
+)
+
+func main() {
+	CliInit()
+	defer cli.Close()
+
+	r := gin.Default()
+	r.GET("/query/:domain/:type/:class/:dns", DnsQuery)
+
+	r.Run("0.0.0.0:8445") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+func CliInit() {
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints:   []string{"localhost:2379"},
+		DialTimeout: 5 * time.Second,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resp, err := cli.Grant(context.TODO(), 5)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	key := "/line/dns/" + zoneName + "/" + lineName + "/" + agentName
+
+	_, err = cli.Put(context.TODO(), key, "online", clientv3.WithLease(resp.ID))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// to renew the lease only once
+	ka, kaerr := cli.KeepAliveOnce(context.TODO(), resp.ID)
+	if kaerr != nil {
+		log.Fatal(kaerr)
+	}
+}
+
+func DnsQuery(c *gin.Context) {
+	dnsType := StrToUint16(c.Param("type"))
+	dnsClass := StrToUint16(c.Param("class"))
+	domain := c.Param("domain")
+	dnsServerStr := c.Param("dns")
+
+	m1 := new(dns.Msg)
+	m1.Id = dns.Id()
+	m1.RecursionDesired = true
+	m1.Question = make([]dns.Question, 1)
+	m1.Question[0] = dns.Question{domain, dnsType, dnsClass}
+
+	dc := new(dns.Client)
+	in, _, _ := dc.Exchange(m1, dnsServer+":53")
+
+	c.String(200, in.String())
+}
+
+func StrToUint16(s string) uint16 {
+	value, _ := strconv.ParseUint(s, 16, 16)
+	return uint16(value) // done!
+}
